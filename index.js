f2b4db5d589ee9ce0280910e495f4d97e82434ee
unknown <joshuahart96@gmail.com>
2023-04-06 17:34:19 +0700

    adding 'luas segitiga' js file, reset button didnt work though, deadline about 6 hr left:(

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //javascript
+//luas segitiga
+function luastri(){
+    const alas =document.getElementById("alas").value;
+    const tinggi = document.getElementById("tinggi").value;
+     luassegitiga = 0.5*alas*tinggi;
+     const result=`${luassegitiga}`;
+
+     const parent = document.getElementById('result');
+     const child = document.createElement('p');
+     child.innerHTML=result;
+    parent.appendChild(child);   
+}
+
+    document.getElementById("reset").onclick = function() {
+    document.getElementById("result").value = "";
+};
